0c2080662fecc12d4159c76563357d5b9616f555
OriCacsire <ocacsiregeronimo@udesa.edu.ar>
2024-04-01 22:59:00 -0300

    Empece con header y footer

--- pi-prog3/src/App.js
+++ pi-prog3/src/App.js
@@ -1,11 +1,23 @@
-import Navbar from "./Components/Navbar/Navbar"
+//Importamos los componentes y los screens
+import {Switch, Route} from "react-router-dom";
+import Header from "./Components/Header/Header";
+import Footer from "./Components/Footer/Footer";
+import Navbar from "./Components/Navbar/Navbar";
+// import Home from "./Screens/Home/Home"
+
 
 function App() {
 
 
   return (
     <div>
+      <Header/>
       <Navbar/>
+
+      <Switch>
+        {/* RUTAS */}
+      </Switch>
+      <Footer/>
     </div>
   );
 }
---
+++ pi-prog3/src/Components/Footer/Footer.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import "./styles.css"
+
+function Footer() {
+
+    return (
+        <footer>
+            <section className='footer'>
+                <article className='names'>
+                    Francisco del Yerro
+                </article>
+                <article className='names'>
+                    Tomás Kearney
+                </article>
+                <article className='names'>
+                    Oriana Cacsire
+                </article>
+            </section>
+        </footer>
+    )
+
+}
+
+export default Footer
