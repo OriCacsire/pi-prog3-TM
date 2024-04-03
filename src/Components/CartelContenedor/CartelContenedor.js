3f0f7672afb9fedcd83f857017d1cc2f13e3d4fa
frandelyerro <fdelyerro@udesa.edu.ar>
2024-04-03 18:42:01 -0300

    maspopulares

--- src/Components/CartelContenedor/CartelContenedor.js
+++ src/Components/CartelContenedor/CartelContenedor.js
@@ -2,7 +2,25 @@ import React, { Component } from 'react'
 import Cartel from '../Cartel/Cartel'
 
  class CartelContenedor extends Component {
+  constructor(props){ 
+    super(this.props)
+    this.state = {
+      cartelera : []
+    }
+  }
+  componentDidMount(){
+    fetch("https://api.themoviedb.org/3/movie/now_playing")
+    .then(resp => resp.json())
+    .then(data =>{
+      for (let i = 0; i < array.length; i++) {
+        
+        
+      }
+    })
+
+  }
   render() {
+   
     return (
       <div>
         
