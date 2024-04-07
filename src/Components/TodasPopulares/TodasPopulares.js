add21481559aa2ac064f9d6d387d950a32f86507
OriCacsire <ocacsiregeronimo@udesa.edu.ar>
2024-04-06 23:51:54 -0300

    descomente todasPopulares porque me tiraba algunos comentarios.

--- src/Components/TodasPopulares/TodasPopulares.js
+++ src/Components/TodasPopulares/TodasPopulares.js
@@ -1,45 +1,45 @@
-// import React, { Component } from 'react'
-// import PopularesContainer from "../PopularesContainer/index"
+import React, { Component } from 'react'
+import PopularesContainer from "../PopularesContainer/index"
 
 
-// export default class TodasPopulares extends Component {
-//     constructor(props) {
-//         super(props)
-//         this.state = {
-//             popular: [],
-//             page: 1
-//         }
-//     }
-//     componentDidMount(){
-//         fetch("https://api.themoviedb.org/3/movie/popular").then(resp => resp.json()).then(data =>{
-//             this.setState ({
-//                 popular : data.results.slice(0,10)
-//             })
-//         })
-//         .catch(err => console.log(err))
+export default class TodasPopulares extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            popular: [],
+            page: 1
+        }
+    }
+    componentDidMount(){
+        fetch("https://api.themoviedb.org/3/movie/popular").then(resp => resp.json()).then(data =>{
+            this.setState ({
+                popular : data.results.slice(0,10)
+            })
+        })
+        .catch(err => console.log(err))
 
-//     }
-//     masPopulares(){
-//         fetch("https://api.themoviedb.org/3/movie/popular?page=" + (this.state.page + 1))
-//         .then(resp => resp.json())
-//         .then(data => {
-//             this.setState({
-//                 popular: this.state.popular.concat(data.results),
-//                 page: this.state.page + 1
-//             })
-//         })
-//         .catch(err => console.log(err))
+    }
+    masPopulares(){
+        fetch("https://api.themoviedb.org/3/movie/popular?page=" + (this.state.page + 1))
+        .then(resp => resp.json())
+        .then(data => {
+            this.setState({
+                popular: this.state.popular.concat(data.results),
+                page: this.state.page + 1
+            })
+        })
+        .catch(err => console.log(err))
 
-//     }
-// render() {
-//     return (
-//       <div>
-//         <h2>
-//             TodasPopulares
-//         </h2>
-//         <PopularesContainer popular = {this.state.popular}/>
-//         <button onClick={this.masPopulares()}>ver mas </button>
-//       </div>
-//     )
-//   }
-// }
+    }
+render() {
+    return (
+      <div>
+        <h2>
+            TodasPopulares
+        </h2>
+        <PopularesContainer popular = {this.state.popular}/>
+        <button onClick={this.masPopulares()}>ver mas </button>
+      </div>
+    )
+  }
+}
