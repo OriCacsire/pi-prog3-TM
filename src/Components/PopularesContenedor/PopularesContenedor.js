79a578890ab240230bd486976ae189e351168e7b
tomikearney <tomikearney@hotmail.com>
2024-04-08 18:31:44 -0300

    Avanzamos con detalle con Ori

--- src/Components/PopularesContenedor/PopularesContenedor.js
+++ src/Components/PopularesContenedor/PopularesContenedor.js
@@ -12,19 +12,19 @@ class PopularesContenedor extends Component {
           this.props.filmPopulares.length === 0 ?
           <h1 className='titles'> Buscando Peliculas ... </h1>
           :
-          this.props.filmPopulares.map((peliPop)=>
+          this.props.filmPopulares.map((peliPop, idx)=>
           
           <Populares
           // Envios las props con la inf que se recupera de la api Cartel
           //peliPop el array
-          key={peliPop.id}
+          key={idx+peliPop}
+          id={peliPop.id}
           imagen ={"https://image.tmdb.org/t/p/w342" + peliPop.poster_path} //Link de img segun consigna y peliPop --> parametro y la prop del api
           title={peliPop.title}
           descripcion = {peliPop.overview}
 
           
           />
-          
           )
         }
 
--- src/Screens/DetallePelicula/DetallePelicula.js
+++ src/Screens/DetallePelicula/DetallePelicula.js
@@ -1,11 +1,40 @@
 import React, { Component } from 'react'
+import {options} from "../../Utils/Constants"
 
 export default class DetallePelicula extends Component {
+
+  constructor(props){
+    super(props)
+    this.state = {
+      filmPopulares: [],
+      id: this.props.match.params.id
+    }
+  }
+  
+
+  componentDidMount(){
+
+      fetch("https://api.themoviedb.org/3/movie/" + this.state.id, options)
+          .then(resp => resp.json())
+          .then(data => {
+              this.setState({
+                  filmPopulares: data
+              })
+              console.log(data);
+          })
+          .catch(error => console.log(error))
+    
+  }
+  
+
   render() {
+
+    
+    
     return (
-      <div>
-        
-      </div>
+      <main>
+        <h2>{this.state.filmPopulares.original_title}</h2>
+      </main>
     )
   }
 }
